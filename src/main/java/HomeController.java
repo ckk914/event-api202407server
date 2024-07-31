b871c3df31fcd6519c1da4afbc3326644773c23a
KyoungkonCho <50573460+ckk914@users.noreply.github.com>
2024-07-31 11:52:16 +0900

    :art: 홈컨트롤러 추가

---
+++ src/main/java/HomeController.java
@@ -0,0 +1,11 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "도커로 구동확인!!";
+    }
+}
